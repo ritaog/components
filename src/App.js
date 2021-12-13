d24e0415f5282d99bd7e41473dbb77954608cbbd
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2021-12-13 00:15:49 -0700

    changes code structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from "react";
+import faker from "faker";
 
 import "./App.css";
 import CommentDetail from "./components/CommentDetail.js";
+import ApprovalCard from "./components/ApprovalCard";
 
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail authorName={"Sam"} />
-      <CommentDetail authorName={"Alex"} />
-      <CommentDetail authorName={"Jane"} />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Nice blog post!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          comment="Lovely post!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:01PM "
+          comment="Quite insightful!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
--- src/components/CommentDetail.js
+++ src/components/CommentDetail.js
@@ -1,20 +1,21 @@
 import React from "react";
-import faker from "faker";
 
-const CommentDetail = ({ authorName }) => {
+const CommentDetail = ({ author, timeAgo, comment, image }) => {
+  //props =  {author: "Sam"}
+  //const {author} = props;
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={image} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          {authorName}
+          {author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{comment}</div>
       </div>
     </div>
   );
